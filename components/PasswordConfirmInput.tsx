96384dc6e333e29d2c84c4808358c756d0bef597
조철민 <xcv9153@gmail.com>
2025-06-02 22:53:59 +0900

    [Inflearn] React Native Expo - 섹션 5진행중

---
+++ components/PasswordConfirmInput.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Controller, useFormContext, useWatch } from "react-hook-form";
+import InputField from "./InputField";
+
+function passwordConfirmInput() {
+  const { control } = useFormContext();
+  const password = useWatch({ control, name: "password" });
+  return (
+    <Controller
+      name="passwordConfirm"
+      control={control}
+      rules={{
+        validate: (data: string) => {
+          if (data !== password) {
+            return "비밀반호가 일치하지 않습니다.";
+          }
+        },
+      }}
+      render={({ field: { ref, onChange, value }, fieldState: { error } }) => (
+        <InputField
+          ref={ref}
+          label="비밀번호 확인"
+          placeholder="비밀번호를 입력해주세요"
+          textContentType="oneTimeCode"
+          secureTextEntry
+          value={value}
+          onChangeText={onChange}
+          error={error?.message}
+        />
+      )}
+    />
+  );
+}
+
+export default passwordConfirmInput;
